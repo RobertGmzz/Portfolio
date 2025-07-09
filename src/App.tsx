272ed2a2a4420a8ffbe555e0c0639c1f9d5281c9
Robert <r0b3rt0gzz@gmail.com>
2025-07-09 03:29:07 -0300

    mejore un poco el diseño general, cree el componente footer y lo importe en app.tsx aunque no contiene estilos aun

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 //import { useState } from 'react'
-import './App.css'
 import './components/Header'
 import { Main } from './components/Main'
 import { Header } from './components/Header'
-//import {Footer} from './components/Footer'
+import {Footer} from './components/Footer'
 
 //funcion que renderizara todos los componentes a main.tsx
 
@@ -12,6 +11,7 @@ function App() {
     <>
     <Header></Header>
     <Main></Main>
+    <Footer></Footer>
     </>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,14 @@
 export function Header(){
-
     return (
-        <header className="flex justify-between items-center p-3 w-[1120px] mx-auto">
+        <header id="#top" className="flex justify-between items-center p-3 w-full xl:w-[1120px] mx-auto">
             <img className="size-12 rounded-full" 
             src="../public/icono.png" 
             alt="robert photo"/>
 
             <nav className="flex text-white gap-x-8">
-                <a href="#">Home</a>
-                <a href="#">About</a>
-                <a href="#">Projects</a>
+                <a href="#top">Home</a>
+                <a href="#about">About</a>
+                <a href="#projects">Projects</a>
                 <a href="#">Contact</a>
             </nav>
         </header>
